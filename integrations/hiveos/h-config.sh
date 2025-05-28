594140f25cbec5da7fa549e91c4e1de83f60bff8
kobradag <168386458+kobradag@users.noreply.github.com>
2025-05-28 14:34:12 -0500

    Update h-config.sh

--- integrations/hiveos/h-config.sh
+++ integrations/hiveos/h-config.sh
@@ -1,21 +1,20 @@
 ####################################################################################
 ###
 ### kobra-miner
-### https://github.com/tmrlvi/kobra-miner/releases
+### https://github.com/kobradag/koda-miner-gpu/releases
 ###
 ### Hive integration: Merlin
 ###
 ####################################################################################
 
 #!/usr/bin/env bash
-[[ -e /hive/custom ]] && . /hive/custom/kobra-miner/h-manifest.conf
-[[ -e /hive/miners/custom ]] && . /hive/miners/custom/kobra-miner/h-manifest.conf
+[[ -e /hive/custom ]] && . /hive/custom/kobra/h-manifest.conf
+[[ -e /hive/miners/custom ]] && . /hive/miners/custom/kobra/h-manifest.conf
 conf=""
-conf+=" --kobra-address=$CUSTOM_URL --mining-address $CUSTOM_TEMPLATE"
+conf+=" --mining-address $CUSTOM_TEMPLATE" -s $CUSTOM_URL
 
 
 [[ ! -z $CUSTOM_USER_CONFIG ]] && conf+=" $CUSTOM_USER_CONFIG"
 
 echo "$conf"
 echo "$conf" > $CUSTOM_CONFIG_FILENAME
-
